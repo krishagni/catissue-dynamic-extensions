5612c9c1c242dfcb41c0872f4c1d233c81e984c2
Vinayak Pawar <vinayakapawar@gmail.com>
2020-01-02 22:18:56 +0530

    OPSMN-5227: Used PV attribute as the cache key to store and retrieve the field's default list.
    The default list populated in the PV fields is cached at the browser side to prevent
    frequent API calls. However, this cached list was incorrectly used for PV fields of
    different attributes i.e. the same default list was used for both specimen_type and
    clinical_diagnosis.
    
    To fix the problem, used PV attribute as the key to index into the cache for storing
    and retrieving the field's default list.

--- software/DynamicExtentions/src/ngweb/js/de-form.js
+++ software/DynamicExtentions/src/ngweb/js/de-form.js
@@ -1866,6 +1866,14 @@ edu.common.de.FileUploadField = function(id, field, args) {
           that.progressBar.removeClass("hidden");
           that.progressBar.children().css('width', pct + '%');
         }
+      },
+
+      error: function(e, data) {
+        if (e) {
+          alert(e.responseText);
+        } else {
+          alert("Unknown file upload error");
+        }
       }
     });
 
@@ -2337,9 +2345,7 @@ edu.common.de.LookupSvc = function(params) {
 
   var defaultValue;
 
-  this.getEntities = function(queryTerm, searchFilters, field) {
-    var deferred = $.Deferred();
-
+  this.defaultCacheKey = function(queryTerm, searchFilters, field) {
     var resultKey = '_default';
     if (!queryTerm) {
       if (searchFilters) {
@@ -2352,11 +2358,24 @@ edu.common.de.LookupSvc = function(params) {
           resultKey += keys[i] + "__" + searchFilters[keys[i]] + '__';
         }
       }
+    }
 
-      if (defaultList[resultKey]) {
-        deferred.resolve(defaultList[resultKey]);
-        return deferred.promise();
-      }
+    return resultKey;
+  }
+
+  this.getEntities = function(queryTerm, searchFilters, field) {
+    var deferred = $.Deferred();
+
+    var resultKey = '';
+    if (typeof this.getCacheKey == 'function') {
+      resultKey = this.getCacheKey(queryTerm, searchFilters, field);
+    } else {
+      resultKey = this.defaultCacheKey(queryTerm, searchFilters, field);
+    }
+
+    if (!queryTerm && defaultList[resultKey]) {
+      deferred.resolve(defaultList[resultKey]);
+      return deferred.promise();
     }
 
     var baseUrl = this.getApiUrl();
@@ -2416,9 +2435,15 @@ edu.common.de.LookupSvc = function(params) {
       }
     }
 
+    var xhr = entitiesMap[id + '_xhr'];
+    if (!xhr) {
+      var baseUrl = this.getApiUrl();
+      xhr = $.ajax({type: 'GET', url: baseUrl + id, headers: this.getHeaders()})
+      entitiesMap[id + '_xhr'] = xhr;
+    }
+
     var that = this;
-    var baseUrl = this.getApiUrl();
-    $.ajax({type: 'GET', url: baseUrl + id, headers: this.getHeaders()})
+    xhr
       .done(function(data) {
         var result = that.formatResult(data);
         entitiesMap[id] = result;
